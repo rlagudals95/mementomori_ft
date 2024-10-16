1639adfc51dcd820a016351e4813b920df45c52e
rlagudals95 <hm.kim@ohoolabs.com>
2024-10-16 21:09:13 +0900

    feat: column & row

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,79 @@
 import 'package:flutter/material.dart';
 
-// --------------------------------------
-// [Import Flutter Webview]
-import 'package:webview_flutter/webview_flutter.dart';
-// --------------------------------------
-// [Import for Android features]
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-// --------------------------------------
-// [Import for iOS features]
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
-// --------------------------------------
-
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _WebViewAppState();
-}
-
-class _WebViewAppState extends State<MyApp> {
-  late final WebViewController controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = WebViewController()
-      ..setBackgroundColor(Colors.black)
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(
-        Uri.parse('https://www.google.co.kr'),
-      )
-      ..setNavigationDelegate(NavigationDelegate(
-        onNavigationRequest: (request) {
-          return NavigationDecision.navigate;
-        },
-        onUrlChange: (UrlChange change) {
-          debugPrint('url change to ${change.url}');
-        },
-        onPageFinished: (String url) {
-          debugPrint('page finished loading: $url');
-        },
-      ));
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue), home: const MyHomepage());
+  }
+}
+
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({super.key});
+
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Flutter Simple Example')),
-      body: WebViewWidget(controller: controller),
-    );
+        // Scaffold(뼈대)로 레이아웃을 만든다.
+        appBar: AppBar(title: const Text('title')),
+        drawer: Drawer(
+            child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text("Drawer Header Part"),
+            ),
+            ListTile(
+              title: Text("Menu 1"),
+            ),
+          ],
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print('press'),
+          child: const Icon(Icons.mouse),
+        ),
+        body: Container(
+          alignment: Alignment.topLeft,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: 200,
+                    color: Colors.blue),
+                Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: 200,
+                    color: Colors.red)
+              ]),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: 200,
+                    color: Colors.purple),
+                Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: 200,
+                    color: Colors.yellow)
+              ])
+            ],
+          ),
+        ));
   }
 }
